02a2f7041a1def8569e75e68e8a211ffdbee793d
jia.fu2013@gmail.com <jia.fu2013@gmail.com>
2021-02-09 20:35:57 +0800

    check Collision

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gametank
 c game based on SDL2 
+
+gcc -fexec-charset=GBK -g main.c -o main.exe -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -ltmx -lxml2 -lz -liconv -lwsock32
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "index.h"
+#include "string.h"
 
 const int WIDTH = 600;
 const int HEIGHT = 600;
@@ -18,10 +19,11 @@ const int LEFT = 180;
 
 
 typedef struct{
-    float x,y;
+    int x,y;
     int width,height;
     int direction;
     int angle;
+    int speed;
     SDL_Texture *texture;
 }Player;
 
@@ -33,6 +35,106 @@ void createPlayer(Player player, SDL_Rect s_rect, SDL_Rect d_rect, SDL_Renderer 
     SDL_RenderCopyEx(render, player.texture, &s_rect, &d_rect, player.angle, NULL, player.direction);
 
     SDL_DestroyTexture(player.texture);
+}
+
+//todo: check player near box is in map box
+int checkCollision(Player player, tmx_map* map, int dicrection)
+{
+    //遍历map图形
+
+    tmx_layer *layer = map->ly_head;
+
+    unsigned long i, j;
+    unsigned int gid, x, y, w, h, react_height, react_width, flags;
+    SDL_Rect player_rect;
+
+    
+
+    tmx_tileset *ts;
+  
+    void *image;
+
+    while (layer)
+    {
+        if (layer->visible)
+        {
+            if (strcmp(layer->name, "brick") == 0)
+            {
+
+                tmx_object_group *objgr = layer->content.objgr;
+
+
+                SDL_Rect rect;
+                set_color(objgr->color);
+                tmx_object *head = objgr->head;
+                while (head)
+                {
+                    if (head->visible)
+                    {
+                        if (head->obj_type == OT_SQUARE)
+                        {
+                            rect.x = head->x;
+                            rect.y = head->y;
+                            rect.w = head->width;
+                            rect.h = head->height;
+
+                            
+                            printf("当前的tank位置x:%d,y:%d\n", player.x,player.y);
+                            printf("rect值x:%d,y:%d,w:%d,h:%d\n",rect.x,rect.y,rect.w,rect.h);
+
+                            // if(player.x+player.speed+player.width > rect.x && rect.x + rect.w > player.x && player.y + player.height > rect.y && rect.y + rect.h > player.y)
+                            // {
+                            //     return 1;
+                            // }
+
+                            
+                            //todo 朝向右的时候，右上，右下，都在矩形中
+                            if (dicrection == SDLK_RIGHT)
+                            {
+                                if ((rect.x < player.x + player.width + player.speed && rect.y <= player.y && rect.x + rect.w > player.x + player.width + player.speed && rect.y + rect.h > player.y) || (rect.x < player.x + player.width + player.speed && rect.y < player.y + player.height && rect.x + rect.w > player.x + player.width + player.speed && rect.y + rect.h > player.y + player.height))
+                                {
+                                    return 1;
+                                }
+                            }
+                            else if (dicrection == SDLK_LEFT)
+                            {
+                                if ((rect.x < player.x - player.speed && rect.y <= player.y && rect.x + rect.w > player.x - player.speed && rect.y + rect.h > player.y) || (rect.x < player.x - player.speed && rect.y < player.y + player.height && rect.x + rect.w > player.x - player.speed && rect.y + rect.h > player.y + player.height))
+                                {
+                                    return 1;
+                                }
+                            }
+                            else if (dicrection == SDLK_UP)
+                            {
+                                if ((rect.x <= player.x && rect.y < player.y - player.speed && rect.x + rect.w > player.x && rect.y + rect.h > player.y - player.speed) || (rect.x < player.x + player.width && rect.y < player.y - player.speed && rect.x + rect.w > player.x + player.width && rect.y + rect.h > player.y - player.speed))
+                                {
+                                    return 1;
+                                }
+                            }
+                            else if(dicrection == SDLK_DOWN)
+                            {
+                                if ((rect.x < player.x + player.width && rect.y < player.y + player.height + player.speed && rect.x + rect.w > player.x + player.width && rect.y + rect.h > player.y + player.height + player.speed) || (rect.x <= player.x && rect.y < player.y + player.height + player.speed && rect.x + rect.w > player.x && rect.y + rect.h > player.y + player.height + player.speed))
+                                {
+                                    return 1;
+                                }
+                            }
+                            
+                        }
+                    }
+                    head = head->next;
+                }
+              
+                
+
+                
+                            
+                        
+                    
+                
+            }
+        }
+        layer = layer->next;
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -81,6 +183,7 @@ int main(int argc, char *argv[])
     player1.height = TANK_HEIGHT;
     player1.direction = SDL_FLIP_NONE;
     player1.angle = 0;
+    player1.speed = 10;
 
     //原矩形
     s_rect.x = 0;
@@ -108,6 +211,7 @@ int main(int argc, char *argv[])
     unsigned int lastTime = 0, currentTime;
     while(1){
         SDL_Event event;
+        int key;
         while (SDL_PollEvent(&event))
         {
             switch (event.type)
@@ -117,38 +221,76 @@ int main(int argc, char *argv[])
                 SDL_Quit();
                 break;
             case SDL_KEYDOWN:
-                
-                if (event.key.keysym.sym == SDLK_RIGHT){
-                    if (d_rect.x < WIDTH - player1.width){
-                        d_rect.x += 10;
-                        player1.angle = 90;
+                key = event.key.keysym.sym;
+
+                if (key == SDLK_RIGHT)
+                {
+                    player1.angle = 90;
+                    int result = checkCollision(player1, map, key);
+
+                    if(result){
+                        break;
+                    }
+
+                    if (d_rect.x < WIDTH - player1.width)
+                    {
+
+                        player1.x += player1.speed;
+                        d_rect.x = player1.x;
+                        
                     }
                 }
                 if (event.key.keysym.sym == SDLK_LEFT)
                 {
+                    player1.angle = 270;
+                    int result = checkCollision(player1, map, key);
+
+                    if (result)
+                    {
+                        break;
+                    }
                     if (d_rect.x > 0)
                     {
-                        d_rect.x -= 10;
-                        player1.angle = 270;
+                        player1.x -= player1.speed;
+                        d_rect.x = player1.x;
+                        
                         
                     }
                 }
 
                 if (event.key.keysym.sym == SDLK_UP)
                 {
+
+                    player1.angle = 0;
+                    int result = checkCollision(player1, map, key);
+
+                    if (result)
+                    {
+                        break;
+                    }
                     if (d_rect.y > 0)
                     {
-                        d_rect.y -= 10;
-                        player1.angle = 0;
+                        player1.y -= player1.speed;
+                        d_rect.y = player1.y;
+                        
                     }
                 }
 
                 if (event.key.keysym.sym == SDLK_DOWN)
                 {
+
+                    player1.angle = 180;
+                    int result = checkCollision(player1, map, key);
+
+                    if (result)
+                    {
+                        break;
+                    }
                     if (d_rect.y < HEIGHT - player1.height)
                     {
-                        d_rect.y += 10;
-                        player1.angle = 180;
+                        player1.y += player1.speed;
+                        d_rect.y = player1.y;
+                        
                     }
                 }
 
